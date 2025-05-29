000011703f1806f56dfe5725f5505840041bc716
bluecroc70 <123221012+bluecroc70@users.noreply.github.com>
2025-05-28 20:03:37 -0700

    Update server.js

--- server.js
+++ server.js
@@ -4,9 +4,8 @@ const server = createServer({
   handler: async (req, res) => {
     const url = new URL(req.url, `http://${req.headers.host}`);
 
-    // Proxy anything under /chatango/ to https://chatango.com
-    if (url.pathname.startsWith('/chatango/')) {
-      // CORS headers to allow frontend fetches
+    // Proxy requests starting with /stchatango/ to https://st.chatango.com
+    if (url.pathname.startsWith('/stchatango/')) {
       res.setHeader('Access-Control-Allow-Origin', '*');
       res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
       res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
@@ -16,10 +15,24 @@ const server = createServer({
         return res.end();
       }
 
-      // Build target URL to Chatango by removing /chatango prefix
+      const targetUrl = 'https://st.chatango.com' + url.pathname.replace('/stchatango', '') + url.search;
+
+      return server.proxy(req, res, targetUrl);
+    }
+
+    // Proxy requests starting with /chatango/ to https://chatango.com (optional)
+    if (url.pathname.startsWith('/chatango/')) {
+      res.setHeader('Access-Control-Allow-Origin', '*');
+      res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
+      res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+      if (req.method === 'OPTIONS') {
+        res.statusCode = 204;
+        return res.end();
+      }
+
       const targetUrl = 'https://chatango.com' + url.pathname.replace('/chatango', '') + url.search;
 
-      // Proxy HTTP or WebSocket request to Chatango
       return server.proxy(req, res, targetUrl);
     }
 
@@ -28,6 +41,7 @@ const server = createServer({
   },
 });
 
-server.listen(3000, () => {
-  console.log('Ultraviolet proxy listening on http://localhost:3000');
+const PORT = 3000;
+server.listen(PORT, () => {
+  console.log(`Ultraviolet proxy listening on http://localhost:${PORT}`);
 });
