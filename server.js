4ae394be5bad25175e9faedf12067066df6075c2
bluecroc70 <123221012+bluecroc70@users.noreply.github.com>
2025-05-28 20:05:51 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,47 +1,23 @@
-const { createServer } = require('@ultraviolet/server');
+const http = require('http');
+const httpProxy = require('http-proxy');
 
-const server = createServer({
-  handler: async (req, res) => {
-    const url = new URL(req.url, `http://${req.headers.host}`);
+const proxy = httpProxy.createProxyServer({});
 
-    // Proxy requests starting with /stchatango/ to https://st.chatango.com
-    if (url.pathname.startsWith('/stchatango/')) {
-      res.setHeader('Access-Control-Allow-Origin', '*');
-      res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
-      res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+const server = http.createServer(function(req, res) {
+  if (req.url.startsWith('/stchatango/')) {
+    req.url = req.url.replace('/stchatango', '');
 
-      if (req.method === 'OPTIONS') {
-        res.statusCode = 204;
-        return res.end();
-      }
-
-      const targetUrl = 'https://st.chatango.com' + url.pathname.replace('/stchatango', '') + url.search;
-
-      return server.proxy(req, res, targetUrl);
-    }
-
-    // Proxy requests starting with /chatango/ to https://chatango.com (optional)
-    if (url.pathname.startsWith('/chatango/')) {
-      res.setHeader('Access-Control-Allow-Origin', '*');
-      res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
-      res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-
-      if (req.method === 'OPTIONS') {
-        res.statusCode = 204;
-        return res.end();
-      }
-
-      const targetUrl = 'https://chatango.com' + url.pathname.replace('/chatango', '') + url.search;
-
-      return server.proxy(req, res, targetUrl);
-    }
-
-    res.statusCode = 404;
-    res.end('Not Found');
-  },
+    proxy.web(req, res, { target: 'https://st.chatango.com', changeOrigin: true }, (e) => {
+      res.writeHead(502);
+      res.end('Proxy error');
+    });
+  } else {
+    res.writeHead(404);
+    res.end('Not found');
+  }
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-  console.log(`Ultraviolet proxy listening on http://localhost:${PORT}`);
+  console.log(`Proxy server listening on port ${PORT}`);
 });
